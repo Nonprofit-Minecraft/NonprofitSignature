0565e6889f1c7f7249d7781d2a157f0148311cf5
Sotr <i@omc.hk>
2019-02-23 21:26:20 +0800

    1.0

---
+++ src/main/java/org/nonprofit_minecraft/signature/NonprofitSignatureAPI.java
@@ -0,0 +1,7 @@
+package org.nonprofit_minecraft.signature;
+
+public abstract class NonprofitSignatureAPI {
+    public static boolean verify() {
+        return true;
+    }
+}
---
+++ src/main/java/org/nonprofit_minecraft/signature/bukkit/NonprofitSignaturePlugin.java
@@ -0,0 +1,15 @@
+package org.nonprofit_minecraft.signature.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NonprofitSignaturePlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        
+    }
+}
