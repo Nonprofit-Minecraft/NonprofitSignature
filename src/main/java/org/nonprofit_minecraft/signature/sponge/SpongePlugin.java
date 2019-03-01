e31e74b770d1c033fe8cb0d6aaf152180d99c24d
Sotr <i@omc.hk>
2019-03-01 17:37:08 +0800

    1.0 @ Supports for Bungeecord / Sponge

---
+++ src/main/java/org/nonprofit_minecraft/signature/sponge/SpongePlugin.java
@@ -0,0 +1,12 @@
+package org.nonprofit_minecraft.signature.sponge;
+
+import org.bstats.sponge.Metrics2;
+import org.spongepowered.api.plugin.Plugin;
+
+import com.google.inject.Inject;
+
+@Plugin(id = "nonprofit_signature", name = "NonprofitSignature", version = "1")
+public class SpongePlugin {
+    @Inject
+    private Metrics2 metrics;
+}
---
+++ src/main/java/org/nonprofit_minecraft/signature/sponge/SpongeSignature.java
@@ -0,0 +1,9 @@
+package org.nonprofit_minecraft.signature.sponge;
+
+import org.nonprofit_minecraft.signature.api.Signatures;
+
+public class SpongeSignature implements Signatures {
+    public boolean verify() {
+        return true;
+    }
+}
